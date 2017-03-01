799a223c3662674cbad9a1d4933a9520cb11d10c
Martin Vestergaard <mvestergaard@gmail.com>
2017-03-01 18:30:56 +0100

    Added initial hash function. Created variables for move patterns

--- script.js
+++ script.js
@@ -290,18 +290,35 @@ Board.prototype.toggleTurn = function() {
     this.turn = this.turn == WHITE ? BLACK : WHITE;
 };
 
+var _piece_primes = [0/*dummy*/,2,3,5,7,11,13,0/**/,0/**/,17,19,23,29,31,37];
+var _primes = [41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383];
+Board.prototype.getHashValue = function(){
+	var i = 0;
+	var hash = 0;
+	for (i; i < 64; i++){
+		hash += _piece_primes[this.array[i]]*_primes[i];
+	}
+	return hash;
+};
+
+var _rook_move_pattern = [[-1,0],[0,1],[1,0],[0,-1]];
+var _knight_move_pattern = [[-1,2],[1,2],[2,1],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1]];
+var _bishop_move_pattern = [[-1,1],[1,1],[1,-1],[-1,-1]];
+var _queen_king_move_pattern = [[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1]];
+var _pawn_move_pattern = {
+    attacks: [[-1,1],[1,1]],
+    movements: [/*if first move*/[0,2],/*otherwise*/[0,1]]
+};
+
 function getMovePatternsForPiece(pieceType){
     switch (pieceType) {
         case PAWN: 
-            return {
-                attacks: [[-1,1],[1,1]],
-                movements: [/*if first move*/[0,2],/*otherwise*/[0,1]]
-            };
-        case ROOK: return [[-1,0],[0,1],[1,0],[0,-1]];
-        case KNIGHT: return [[-1,2],[1,2],[2,1],[2,-1],[1,-2],[-1,-2],[-2,-1],[-2,1]];
-        case BISHOP: return [[-1,1],[1,1],[1,-1],[-1,-1]];
+            return _pawn_move_pattern;
+        case ROOK: return _rook_move_pattern;
+        case KNIGHT: return _knight_move_pattern;
+        case BISHOP: return _bishop_move_pattern;
         case QUEEN: 
-        case KING: return [[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1]];
+        case KING: return _queen_king_move_pattern;
     }
 }
 
